4b383fcec687f6b248bcf582b1860ff8174456cd
luizsoave <luiz_gsv1@hotmail.com>
2024-06-28 11:14:15 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,7 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Boas vindas ao Allbooks")
+  console.log("Essa é uma API de uma loja de livros online")
+  console.log("Acesse a API em http://localhost:8000")
 })
